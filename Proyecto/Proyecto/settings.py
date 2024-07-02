dc461c0a56b62e0dc6bbc0ad31e57eea8230ae09
león-cs02 <leonkceres880@gmail.com>
2024-07-02 16:25:06 -0300

    agregamos la aplicacion

--- Proyecto/Proyecto/settings.py
+++ Proyecto/Proyecto/settings.py
@@ -31,12 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #apps de django
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #apps de proyecto 
+    'aplicacion',
 ]
 
 MIDDLEWARE = [
